332d24df6db40542317babf6359fffd2df6c59b7
nova <courtois1337@gmail.com>
2020-02-09 17:10:25 -0600

    add copy ability

--- client/src/components/card/index.js
+++ client/src/components/card/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './style.scss';
 
+import clipboard from '../../img/clipboard.svg';
+
 export default function Card(props) {
   const backgroundStyle = {
     backgroundImage: props.value
   };
+  const copyText = `background-image: ${props.value};`;
 
   return (
     <>
       <div class="card" style={backgroundStyle}>
         <div class="card__container">
-          {props.name}
+          <div class="card__content">
+            <a className="card__copy" href="" onClick={(e) => {e.preventDefault();navigator.clipboard.writeText(copyText)}}>
+              <img src={clipboard} className="card__clipboard" alt={`copy ${props.name} flag`} />
+            </a>
+            <div className="card__flag-name">
+              {props.name} flag
+            </div>
+          </div>
         </div>
       </div>
     </>
--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.scss';
 
-import logo from '../../logo.svg';
+import logo from '../../img/logo.svg';
 
 export default function Header(props) {
   return (
