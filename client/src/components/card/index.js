36d2b12764e98f743aa1ebf15986d57a35a7b977
nova <courtois1337@gmail.com>
2020-02-10 19:54:08 -0600

    set up cards and transitions

--- client/src/components/card/index.js
+++ client/src/components/card/index.js
@@ -11,13 +11,14 @@ export default function Card(props) {
 
   return (
     <>
-      <div class="card" style={backgroundStyle}>
+      <div class="card">
         <div class="card__container">
           <div class="card__content">
             <a className="card__copy" href="" onClick={(e) => {e.preventDefault();navigator.clipboard.writeText(copyText)}}>
               <img src={clipboard} className="card__clipboard" alt={`copy ${props.name} flag`} />
             </a>
-            <div className="card__flag-name">
+            <div className="card__flag" style={backgroundStyle}></div>
+            <div className="card__title">
               {props.name} flag
             </div>
           </div>
