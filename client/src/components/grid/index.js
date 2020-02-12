af7afbba841f7cebae7df64aa7fcd95680841e25
nova <courtois1337@gmail.com>
2020-02-11 19:58:03 -0600

    convert exported variables to map

--- client/src/components/grid/index.js
+++ client/src/components/grid/index.js
@@ -4,13 +4,24 @@ import './style.scss';
 import Card from '../card';
 
 import gaydients from '../../gaydients.scss';
-let gaydientItems = [];
+let gaydientItems = {};
 
 Object.keys(gaydients).forEach(key=>{
-   gaydientItems.push({
-     'name': key,
-     'value': gaydients[key]
-   })
+  const item = key.split('--');
+  const name = item[0];
+  const type = item[1];
+  const value = gaydients[key];
+
+  let instance = {};
+  instance[type] = value;
+
+  if (name in gaydientItems) {
+    gaydientItems[name].push(instance);
+  }
+  else {
+    gaydientItems[name] = [];
+    gaydientItems[name].push(instance);
+  }
 });
 
 export default function Grid(props) {
@@ -18,13 +29,7 @@ export default function Grid(props) {
     <>
       <div className="grid">
         <div className="grid__container">
-          {
-            gaydientItems.map((item, i) => {
-              return(
-                <Card name={item.name} value={item.value} key={i} />
-              )
-            })
-          }
+
         </div>
       </div>
     </>
