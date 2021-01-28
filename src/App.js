20c7da939758fe1d903452eacfe3138da4b1ee67
nova <courtois1337@gmail.com>
2021-01-27 17:44:08 -0800

    remove ga

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import ReactGA from 'react-ga';
 import cookie from 'react-cookies';
 
 import CookieBanner from './components/cookie-banner';
@@ -13,27 +12,10 @@ function App() {
   const cookieConsent = cookie.load(cookieConsentName);
   const [hasCookieResponse, setCookieResponse] = useState(cookieConsent);
 
-  const initializeCookieConsent = () => {
-    ReactGA.initialize('UA-158649316-1');
-    ReactGA.pageview('/');
-  };
-
   const giveCookieConsentResponse = (gaveConsent) => {
     cookie.save(cookieConsentName, gaveConsent, { path: '/' });
     setCookieResponse(gaveConsent);
-
-    if (gaveConsent) {
-      initializeCookieConsent();
-      ReactGA.event({
-        category: 'Cookie Consent',
-        action: 'Click Okay'
-      });
-    }
   };
-
-  if (cookieConsent) {
-    initializeCookieConsent();
-  }
 
   // handle scroll event
   const scrollClass = 'is-scrolled';
